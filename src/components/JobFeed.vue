e60ac4cbba2b001f584ac96cd0c953a97a868513
sandra yukna <sandy.blue.bee@gmail.com>
2023-11-17 19:39:21 +0100

    Initial database added

--- src/components/JobFeed.vue
+++ src/components/JobFeed.vue
@@ -1,8 +1,25 @@
 <template>
   <h1>JOB FEED</h1>
+  <p>{{ jobs.first }}</p>
+  <JobPost />
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue'
+import JobPost from './JobPost.vue'
+
+// Data
+const jobs = ref([])
+
+// Lifecycle hooks
+onMounted(() => {
+  console.log("job feed")
+  fetch('http://localhost:3000/jobs')
+  .then((response) => response.json())
+  .then(data => jobs.value = data)
+  .catch(error => console.log(error.message))
+})
+
 
 </script>
 
