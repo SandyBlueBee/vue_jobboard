0cea3472009862a3f1443718037cde8a731c5584
sandra yukna <sandy.blue.bee@gmail.com>
2023-11-17 18:20:46 +0100

    vue jobboard created

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <JobFeed />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import JobFeed from './components/JobFeed.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    JobFeed
   }
 }
 </script>
